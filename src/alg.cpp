f0300aa42c160ddc5f3668443fd30333bdf4991b
ardaelam <artem.elesin2007@gmail.com>
2026-03-24 18:35:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,74 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                ++count;
+            }
+        }
+    }
+    return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            int leftVal = arr[left];
+            int leftCount = 0;
+            while (left <= right && arr[left] == leftVal) {
+                ++leftCount;
+                ++left;
+            }
+            int rightVal = arr[right];
+            int rightCount = 0;
+            while (right >= left && arr[right] == rightVal) {
+                ++rightCount;
+                --right;
+            }
+            count += leftCount * rightCount;
+        } else if (sum < value) {
+            ++left;
+        } else {
+            --right;
+        }
+    }
+    return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        int target = value - arr[i];
+        if (target < 0) continue;
+        int left = i + 1, right = len - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        first = left;
+        if (first >= len || arr[first] != target) continue;
+        left = i + 1, right = len - 1;
+        int last = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        last = right;
+        count += (last - first + 1);
+    }
+    return count;
 }
